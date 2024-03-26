a0982ce78182b26d53cfa4ac1a2199cb79eed569
Varsha84952 <varshitha@vume.in>
2024-03-26 13:50:52 +0530

    journey timeline section updated

--- js/script.js
+++ js/script.js
@@ -218,6 +218,35 @@ win.resize(resizeHandler());
 
 /*---- isotope mobile view ends here -----*/
 
+/*---- Timeline starts from here ----*/
+
+var elements = document.querySelectorAll('.elStoryColumn');
+console.log("elements --- ", elements);
+// Function to check for fade effect on scroll and resize
+function checkForFade() {
+var windowHeight = window.innerHeight;
+elements.forEach(function (element) {
+    var elementHeight = element.offsetHeight;
+    var elementOffset = element.getBoundingClientRect().top;
+    var space = windowHeight - (elementHeight + elementOffset - window.pageYOffset);
+
+    if (space < 80) {
+    element.classList.add('non-focus');
+    } else {
+    element.classList.remove('non-focus');
+    }
+});
+}
+
+// Add event listeners for scroll and resize and call the checkForFade function
+window.addEventListener('scroll', checkForFade);
+window.addEventListener('resize', checkForFade);
+
+// Trigger the scroll event on initial load
+window.dispatchEvent(new Event('scroll'));
+
+/*---- Timeline ends here ----*/
+
   /*----- footer backtotop starts here ------*/
 
   var btn = $('#back-top');
